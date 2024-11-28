1b9d6be4202cab9d709e1682823f4c23afa5d29a
Diego Vargas <diegovs0504@gmail.com>
2024-11-28 13:15:24 -0400

    Implemented Satoshi font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-satoshi)", "sans-serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
