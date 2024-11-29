433e78b60b87885e8234692511cb8ff4eae71774
Diego Vargas <diegovs0504@gmail.com>
2024-11-28 20:00:50 -0400

    Added Header to App

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Header from "@/containers/header";
+
 export default function Home() {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+      <header>
+        <Header />  
+      </header>
+      <main className={'w-screen h-screen bg-black'}>
+
+      </main>
+    </>
   );
 }
