8196d2128bc89e450feddb32a9a050445d27f4bc
Diego Vargas <diegovs0504@gmail.com>
2024-11-30 14:49:13 -0400

    Added Hover and Active transitions to Button Component

--- components/button.tsx
+++ components/button.tsx
@@ -11,21 +11,26 @@ export default function Button({ text, icon, theme, className }: Props) {
       className={`
       ${className}
       ${theme === "dark" ?
-          "bg-[var(--bg-dark)] text-[var(--text-light)]" :
-          "bg-[--bg-light] text-[var(--text-dark)] border"} 
+          "bg-[var(--bg-dark)] text-[var(--text-light)] hover:opacity-80 active:scale-[0.9]" :
+          "bg-[--bg-light] text-[var(--text-dark)] border hover:bg-[var(--text-dark)] hover:text-[var(--text-light)] hover:border-transparent active:scale-[0.9]"} 
       flex
       flex-row
       gap-3
       items-center 
       justify-center
-      py-3
-      px-4
+      py-2
+      px-3
+      md:py-3
+      md:px-4
       rounded-lg
-      text-xl
+      text-base
+      md:text-base
       font-medium
+      transition
+      duration-500
     `}>
       {text}
-      <span className="bg-[var(--primary-button)] p-2 rounded-full">
+      <span className="bg-[var(--primary-button)] px-2 py-0.5 rounded-full">
         {icon}
       </span>
     </button>
