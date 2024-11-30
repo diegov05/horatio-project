6fc9d7da6543e4f500131f57c06d28dc5d9a6f17
Diego Vargas <diegovs0504@gmail.com>
2024-11-30 14:52:42 -0400

    Exported Sections

---
+++ containers/index.ts
@@ -0,0 +1,15 @@
+import CaseStudy from "@/containers/caseStudy";
+import CTA from "@/containers/cta";
+import Footer from "@/containers/footer";
+import Header from "@/containers/header";
+import Hero from "@/containers/hero";
+import Scroller from "@/containers/scroller";
+
+export {
+  CaseStudy,
+  CTA,
+  Footer,
+  Header,
+  Hero,
+  Scroller
+}
